ba858bc51f4a26176d9d7fc8dcaa375f61d5a433
Uzair Imtiaz <107175205+Uzair-Imtiaz@users.noreply.github.com>
2022-08-01 21:54:23 +0500

    Create sp_tester.py

---
+++ sp_tester.py
@@ -0,0 +1,33 @@
+import time
+import random
+
+def mistake(test, input):
+    error = 0
+    for i in range(len(test)):
+        try:
+            if test[i] != input[i]:
+                error += 1
+        except:
+            error += 1
+    return error
+
+def speed(stime, etime, input):
+    time_dif = round(etime - stime)
+    spd = len(input) / time_dif
+    return round(spd)
+
+test = ['Enter the size of list',
+'Enter the integer elements of list',
+'Try the new cross platform']
+test1 = random.choice(test)
+print("Typing test")
+print(test1)
+print('''
+
+''')
+time_s = time.time()
+usertest = input("start typing: ")
+time_e = time.time()
+print()
+print('speed: ', speed(time_s,time_e,usertest), 'w/sec' )
+print('Mistakes: ', mistake(test1, usertest))
